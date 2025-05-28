d5814f75b275fbce9cf4c5f079361710bf6f7a7b
Veljko Skarich <veljko.skarich@gmail.com>
2025-05-28 03:44:26 -0400

    Refactored test and train functions.

--- main_cls.py
+++ main_cls.py
@@ -1,19 +1,14 @@
 from __future__ import print_function
 import warnings
+
+from PVT_forked_repo.PVT_forked.trainer import Trainer
+
 # ignore everything
 warnings.filterwarnings("ignore")
 import os
 import argparse
 import torch
-import torch.optim as optim
-from torch.optim.lr_scheduler import CosineAnnealingLR
-from data import ModelNetDataLoader
-from model.pvt import pvt
-import numpy as np
-from torch.utils.data import DataLoader
 from util import cal_loss, IOStream
-import sklearn.metrics as metrics
-import provider
 
 def _init_():
     if not os.path.exists('checkpoints'):
@@ -24,144 +19,6 @@ def _init_():
     os.system('cp model/pvt.py checkpoints' + '/' + args.exp_name + '/' + 'pvt.py.backup')
     os.system('cp util.py checkpoints' + '/' + args.exp_name + '/' + 'util.py.backup')
     os.system('cp data.py checkpoints' + '/' + args.exp_name + '/' + 'data.py.backup')
-
-
-def train(args, io):
-    print("Training Run Starting....")
-    train_loader = DataLoader(ModelNetDataLoader(partition='train', npoint=args.num_points), num_workers=32,
-                              batch_size=args.batch_size, shuffle=True, drop_last=True)
-    test_loader = DataLoader(ModelNetDataLoader(partition='test', npoint=args.num_points), num_workers=32,
-                             batch_size=args.test_batch_size, shuffle=False, drop_last=False)
-
-    device = torch.device(args.device)
-
-    # Try to load models
-    if args.model == 'pvt':
-        model = pvt().to(device)
-    else:
-        raise Exception("Not implemented")
-
-    print("Let's use", torch.cuda.device_count(), "GPUs!")
-
-    if args.use_sgd:
-        print("Use SGD")
-        opt = optim.SGD(model.parameters(), lr=args.lr * 10, momentum=args.momentum, weight_decay=1e-4)
-    else:
-        print("Use Adam")
-        opt = optim.Adam(model.parameters(), lr=args.lr, weight_decay=1e-4)
-
-    scheduler = CosineAnnealingLR(opt, args.epochs, eta_min=args.lr)
-    criterion = cal_loss
-    best_test_acc = 0
-
-    for epoch in range(args.epochs):
-        scheduler.step()
-        ####################
-        # Train
-        ####################
-        model.train()
-        for data, label in train_loader:
-            data = data.numpy()
-            data = provider.random_point_dropout(data)
-            data[:, :, 0:3] = provider.random_scale_point_cloud(data[:, :, 0:3])
-            data[:, :, 0:3] = provider.shift_point_cloud(data[:, :, 0:3])
-            data = torch.Tensor(data)
-            label = torch.LongTensor(label[:, 0].numpy())
-            data, label = data.to(device), label.to(device).squeeze()
-            data = data.permute(0, 2, 1)
-            opt.zero_grad()
-            logits = model(data)
-            loss = criterion(logits, label)
-            loss.backward()
-            opt.step()
-
-        ####################
-        # Test
-        ####################
-        test_loss = 0.0
-        count = 0.0
-        model.eval()
-        test_pred = []
-        test_true = []
-        for data, label in test_loader:
-            label = torch.LongTensor(label[:, 0].numpy())
-            data, label = data.to(device), label.to(device).squeeze()
-            data = data.permute(0, 2, 1)
-            batch_size = data.size()[0]
-            logits = model(data)
-            loss = criterion(logits, label)
-            preds = logits.max(dim=1)[1]
-            count += batch_size
-            test_loss += loss.item() * batch_size
-            test_true.append(label.cpu().numpy())
-            test_pred.append(preds.detach().cpu().numpy())
-        test_true = np.concatenate(test_true)
-        test_pred = np.concatenate(test_pred)
-        test_acc = metrics.accuracy_score(test_true, test_pred)
-        avg_per_class_acc = metrics.balanced_accuracy_score(test_true, test_pred)
-        outstr = 'Test %d, loss: %.6f, test acc: %.6f, test avg acc: %.6f' % (epoch,
-                                                                              test_loss * 1.0 / count,
-                                                                              test_acc,
-                                                                              avg_per_class_acc)
-        io.cprint(outstr)
-        if test_acc >= best_test_acc:
-            best_test_acc = test_acc
-            torch.save(model.state_dict(), 'checkpoints/%s/model.t7' % args.exp_name)
-
-
-def test(args, io):
-    print("Testing Run Starting....")
-    import time
-
-    test_loader = DataLoader(ModelNetDataLoader(partition='test', npoint=args.num_points), num_workers=args.num_workers,
-                             batch_size=args.test_batch_size, shuffle=False, drop_last=False)
-
-    device = torch.device(args.device)
-
-    # Try to load models
-    start = time.perf_counter()
-
-    model = pvt().to(device)
-    model.load_state_dict(torch.load(args.model_path, map_location=device))
-    end = time.perf_counter()
-
-    print(f"Model loading took {end - start:.4f} seconds")
-
-    model = model.eval()
-    test_true = []
-    test_pred = []
-    first_batch_loaded = False
-    with torch.no_grad():
-        start = time.perf_counter()
-        for data, label in test_loader:
-            if not first_batch_loaded:
-                print("Processing Batches....")
-                first_batch_loaded = True
-                end = time.perf_counter()
-                print(f"Lazy data loading took {end - start:.4f} seconds")
-
-            # a) extract scalar label
-            label = torch.LongTensor(label[:, 0].numpy())
-            # b) move to device
-            data, label = data.to(device), label.to(device).squeeze()
-            # c) reshape to (B, C, N) ← originally (B, N, C)
-            # (B, C, N) == (Batch, Channels, Points) == (8, 6, 1024)
-            data = data.permute(0, 2, 1)
-
-            # d) model(data) invokes the forward method of the pvt class
-            logits = model(data)
-
-            # e) take argmax over classes
-            preds = logits.max(dim=1)[1]
-            test_true.append(label.cpu().numpy())
-            test_pred.append(preds.detach().cpu().numpy())
-
-        test_true = np.concatenate(test_true)
-        test_pred = np.concatenate(test_pred)
-        test_acc = metrics.accuracy_score(test_true, test_pred)
-        avg_per_class_acc = metrics.balanced_accuracy_score(test_true, test_pred)
-        outstr = 'Test :: test acc: %.6f, test avg acc: %.6f' % (test_acc, avg_per_class_acc)
-        io.cprint(outstr)
 
 def set_device(args, io):
     if not args.no_cuda and torch.cuda.is_available():
@@ -227,8 +84,9 @@ if __name__ == "__main__":
     io.cprint(str(args))
 
     torch.manual_seed(args.seed)
+    trainer = Trainer(args, io)
 
     if not args.eval:
-        train(args, io)
+        trainer.train()
     else:
-        test(args, io)
+        trainer.test()
