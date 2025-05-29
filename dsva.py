Fixed shape bug in voxel feature averaging.

dsva.py
@@ -8,11 +8,11 @@ Functions:
 - knn_filter_empty: kNN among non-empty voxels only (handles cases where #non-empty voxels < k).
 - build_edge_features: Concatenate anchor feats, neighbor feats, and normalized offsets.
 - EdgeScorer: Simple 2-layer MLP to score edge features.
+
 """
 
 import torch
 import torch.nn as nn
-
 
 def knn_filter_empty(voxel_coords: torch.Tensor,
                      voxel_feats: torch.Tensor,

trainer.py
@@ -91,10 +91,20 @@ class Trainer():
 
     def train(self):
         print("Training Run Starting....")
-        train_loader = DataLoader(ModelNetDataLoader(partition='train', npoint=self.args.num_points), num_workers=32,
-                                  batch_size=self.args.batch_size, shuffle=True, drop_last=True)
-        test_loader = DataLoader(ModelNetDataLoader(partition='test', npoint=self.args.num_points), num_workers=32,
-                                 batch_size=self.args.test_batch_size, shuffle=False, drop_last=False)
+        train_loader = DataLoader(ModelNetDataLoader(
+            partition='train',
+            npoint=self.args.num_points),
+            num_workers=self.args.num_workers,
+            batch_size=self.args.batch_size,
+            shuffle=True,
+            drop_last=True)
+        test_loader = DataLoader(ModelNetDataLoader(
+            partition='test',
+            npoint=self.args.num_points),
+            num_workers=self.args.num_workers,
+            batch_size=self.args.test_batch_size,
+            shuffle=False,
+            drop_last=False)
 
         device = torch.device(self.args.device)
 