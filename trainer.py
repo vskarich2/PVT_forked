f61abb81d340d45ec1ce0aec9c925809f60ca4a0
Veljko Skarich <veljko.skarich@gmail.com>
2025-05-31 05:42:12 -0400

    Fixed progress bar.

--- trainer.py
+++ trainer.py
@@ -159,8 +159,8 @@ class Trainer():
             running_avg = running_loss / running_count
 
             train_bar.set_postfix({
-                "Avg Loss": f"🔥{running_avg:.6f}🔥",
-                "Batch Loss": f"{curr_loss:.6f}",
+                "🔥Avg Loss": f"{running_avg:.4f}🔥",
+                "Batch Loss": f"{curr_loss:.4f}",
             })
 
         # Close training bar for this epoch
