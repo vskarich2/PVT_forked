436359cf789bbd0fc71f4ea1fca9fca4552dcfc0
Veljko Skarich <veljko.skarich@gmail.com>
2025-06-08 18:37:58 -0400

    Experiment.

--- cs231n/training/saliency.py
+++ cs231n/training/saliency.py
@@ -152,6 +152,7 @@ class SaliencyMixin(VoxelGridCentersMixin):
                 # 2) Isolate one sample and re-run forward from scratch
                 feat_i = feats[i:i + 1].detach().requires_grad_(True)  # (1, C_in, N)
                 out_i  = self.model(feat_i)
+                print("Got here hi")
                 pred_i = out_i.argmax(dim=1).item()  # scalar prediction
                 print(f"  [Batch {batch_idx}, Sample {i}]   pred_i={pred_i}")
 
